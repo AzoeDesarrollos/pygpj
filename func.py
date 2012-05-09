dbb1126a6c7f418368caa58c85f47fda4e560fb6
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@a65c8e69-35cd-dee6-1778-adaf38b9f6a1>
2012-05-09 02:16:26 +0000

    La nueva ver es ahora la ver oficial. La anterior versión ha sido discontinuada. S

---
+++ func.py
@@ -0,0 +1,290 @@
+﻿# coding=UTF-8
+from random import randint
+import os
+import csv
+
+def leerCSV (archivo):
+    '''Lee archivos CSV y los devuelve como una lista.'''
+    
+    arch = csv.reader(open(archivo),dialect='myCSV')
+    temp = []
+    for t in arch:
+        temp.append(t)
+    if len(temp) == 1:
+        temp = temp[0]
+    return temp
+
+def SelCla(claseprevia):
+    '''Provee un selector de clases.'''
+    
+    if claseprevia == '':
+        print ('Elije una clase para este nivel')
+    else:
+        print ('Elije una clase para este nivel [Enter: '+claseprevia+']')
+    CLASES = ('Barbaro','Bardo','Brb','Brd','Bárbaro','Clerigo','Clr','Clérigo','Drd','Druida','Exp','Explorador','Gue','Guerrero','Hcr','Hechicero','Mag','Mago','Mnj','Monje','Paladin','Paladín','Pcr','Picaro','Pld','Pícaro','barbaro','bardo','brb','brd','bárbaro','clerigo','clr','clérigo','drd','druida','exp','explorador','gue','guerrero','hcr','hechicero','mag','mago','mnj','monje','paladin','paladín','pcr','picaro','pld','pícaro','')
+    while True:
+        cla = input('Clase: ')
+        if cla not in CLASES:
+            print ('\nSeleccione una clase válida.')
+        elif cla == '':
+            if claseprevia == '':
+                print ('\nDebe seleccionar una clase')
+            else:
+                cla = claseprevia
+                break
+        else:
+            break
+    
+    CLASES = ('Brb','Brd','Clr','Drd','Exp','Gue','Hcr','Mag','Mnj','Pld','Pcr')
+    if cla in CLASES: Clase = cla
+    elif cla.capitalize() in CLASES: Clase = cla.capitalize()
+    elif cla == '': Clase = ''
+    elif cla in ('Bárbaro','bárbaro','barbaro','Barbaro'): Clase = 'Brb'
+    elif cla in ('Bardo','bardo'): Clase = 'Brd'
+    elif cla in ('Clérigo','clérigo','clerigo','Clerigo'): Clase = 'Clr'
+    elif cla in ('Druida','druida'): Clase = 'Drd'
+    elif cla in ('Explorador','explorador'): Clase = 'Exp'
+    elif cla in ('Guerrero','guerrero'): Clase = 'Gue'
+    elif cla in ('Hechicero','hechicero'): Clase = 'Hcr'
+    elif cla in ('Mago','mago'): Clase = 'Mag'
+    elif cla in ('Monje','monje'): Clase = 'Mnj'
+    elif cla in ('Paladín','paladín','paladin','Paladin'): Clase = 'Pld'
+    elif cla in ('Pícaro','pícaro','picaro','Picaro'): Clase = 'Pcr'
+    return Clase
+
+def ProCla(lista_de_clases,clase,nv_cls):
+    '''Procesa la lista de clases y otiene ATKbase, y TSs.'''
+    
+    nom = lista_de_clases[0]
+    ATKb = lista_de_clases[1]
+    Fort = lista_de_clases[2]
+    Ref = lista_de_clases[3]
+    Vol = lista_de_clases[4]
+    if ATKb[nom.index(clase)] == 'b':
+        A = 1
+    elif ATKb[nom.index(clase)] == 'i':
+        A = 3/4
+    else:
+        A = 1/2
+    
+    if Fort[nom.index(clase)] == 'b':
+        F = 1/2
+        if nv_cls == 1:
+            F += 2
+    else:
+        F = 1/3
+    
+    if Ref[nom.index(clase)] == 'b':
+        R = 1/2
+        if nv_cls == 1:
+            R += 2
+    else:
+        R = 1/3
+    
+    if Vol[nom.index(clase)] == 'b':
+        V = 1/2
+        if nv_cls == 1:
+            V += 2
+    else:
+        V = 1/3
+    
+    return A,F,R,V
+
+def PuntHab (lista_de_clases,clase,nivel,INT_mod,subtipo):
+    '''Devuelve los puntos de habilidad a repartir para el nivel de clase.'''
+    
+    nom = lista_de_clases[0]
+    PHs = lista_de_clases[6]
+    for i in range(len(PHs)):
+        PHs[i] = int(PHs[i])
+    PH = PHs[nom.index(clase)]+INT_mod
+    if nivel == 1:
+        PH *= 4
+        if subtipo == 'humano':
+            PH += 4
+    else:
+        if subtipo == 'humano':
+            PH += 1
+    return PH
+
+def Claseas (clase):
+    '''Devuelve las habilidades cláseas de la clase citada.'''
+    
+    hab_cls = {'Brb':(1,14,17,20,22,36,38,40,41),'Brd':(1,2,5,6,7,8,9,10,11,12,13,14,16,19,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,39,41),'Clr':(1,5,6,8,23,26,30,32,35,37),'Drd':(1,3,5,6,8,14,20,22,23,33,37,38,40,44),'Exp':(1,3,4,5,13,14,20,21,22,23,28,29,33,36,37,38,40,41,43,44),'Gue':(1,17,20,22,23,36,40,41),'Hcr':(1,5,6,10,23,26),'Mag':(1,5,6,7,23,26,27,28,29,30,31,32,33,34,35),'Mnj':(1,2,3,5,8,11,12,13,14,16,21,22,23,24,26,35,36,41),'Pld':(1,2,5,8,20,23,34,35,37,40),'Pcr':(0,1,4,7,8,9,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25,31,36,39,41,42,43)}
+    cls = []
+    for i in hab_cls[clase]:
+        cls.append(habs[i])
+    return cls
+
+def RepRNG (PH,nv_cls,hab_cla,habilidades):
+    '''Devuelve un diccionario con la habilidad y sus rangos.'''
+    
+    os.system(['clear','cls'][os.name == 'nt'])
+    print('\nTienes '+str(PH)+' puntos de habilidad para distribuir en este nivel.\n')
+    b = ''
+    if input('Deseas conocer tus habilidades de clase?\n').lower().startswith('s'):
+        for hab in hab_cla:
+            b = b+hab+', '
+        print(b.rstrip(', ')+'.\n')
+    print ('Recuerda que cualquier habildiad transclásea cuesta dos puntos en lugar de uno.\nEscribe una habilidad y los puntos que desees invertir en ella.\n')
+    
+    rng_max = nv_cls+3
+    rng_max_tc = round(rng_max/2)
+    rng = {}
+    while PH > 0:
+        hab = input('\nHabilidad: ').rstrip(' ').capitalize()
+        if hab not in habilidades:
+            print('Por favor, escribe la habilidad correctamente')
+            hab = input('\nHabilidad: ').rstrip(' ').capitalize()
+        rng[hab]=0
+        puntos = input('Puntos: ')
+        while not puntos.isnumeric():
+            print ('Los rangos deben ser numéricos')
+            puntos = input('Puntos: ')
+        puntos = int(puntos)
+        
+        if hab not in hab_cla:
+            PH -= puntos
+            rng[hab] += puntos/2
+            print('\nPuntos restantes: '+str(PH))
+            if PH < 0:
+                print('no alcanzan los puntos')
+                PH += puntos*2
+                rng[hab] -= puntos/2
+        else:
+            if rng[hab] + puntos > rng_max:
+                print(hab+' ha alcanzado el rango máximo ('+str(rng_max)+').')
+                PH -= rng_max
+                rng[hab] += rng_max
+                print('\nPuntos restantes: '+str(PH))
+                if PH < 0:
+                    print('no alcanzan los puntos')
+                    PH += rng_max
+                    rng[habs.index(hab)] -= rng_max
+            else:
+                PH -= puntos
+                rng[hab] += puntos
+                print('\nPuntos restantes: '+str(PH))
+                if PH < 0:
+                    print('no alcanzan los puntos')
+                    PH += puntos
+                    rng[hab] -= puntos
+    return rng
+
+def HabMod(mods,hab_num,mods_de_caract):
+    '''Calcula el modificador final de habilidad.'''
+    
+    mod = 0
+    temp = mods[hab_num]
+    if temp == 'FUE': mod = mods_de_caract[0]
+    elif temp == 'DES': mod = mods_de_caract[1]
+    elif temp == 'CON': mod = mods_de_caract[2]
+    elif temp == 'INT': mod = mods_de_caract[3]
+    elif temp == 'SAB': mod = mods_de_caract[4]
+    elif temp == 'CAR': mod = mods_de_caract[5]
+    return rng[hab_num]+mod+rcl[hab_num]+sng[hab_num]+dts[hab_num]+obj[hab_num]
+
+def SelDot (lista_de_dotes,nivel):
+    '''Provee un selector de dotes.'''
+    
+    nom = lista_de_dotes[0]
+    pre = lista_de_dotes[1]
+    des = lista_de_dotes[2]
+    if nivel in (1,3,6,9,12,15,18):
+        print ('\nEn el '+str(nivel)+'º nivel, tienes una dote para elegir')
+        dt = input('\nDote: ').rstrip(' ').capitalize()
+        while dt not in nom:
+            print ('Por favor, escribe la dote correctamente\n')
+            dt = input('\nDote: ').rstrip(' ').capitalize()
+        print ('Prerrequisitos: '+pre[nom.index(dt)]+'\n'+des[nom.index(dt)])
+        if input('\n¿Esta seguro?').lower().startswith('s'):
+            return dt
+    else:
+        pass
+
+def UnaCar ():
+    '''Un simple generador para una caracterítica.'''
+    
+    car = [randint(1,6),randint(1,6),randint(1,6),randint(1,6)]
+    car.sort(reverse=True)
+    del car[-1]
+    Car = sum(car)
+    return Car
+    
+def GenTir():
+    ''''Genera las 7 tiradas y descarta la más baja.
+    
+    También, imprime la lista en un lindo formato.'''
+    
+    A,B,C = UnaCar(),UnaCar(),UnaCar()
+    D,E,F = UnaCar(),UnaCar(),UnaCar()
+    G = UnaCar()
+    TirList = [A,B,C,D,E,F,G]
+    TirList.sort(reverse=True)
+    del TirList[-1]
+    
+    imprimir = ''
+    for elemento in TirList:
+        imprimir = imprimir+str(elemento)+', '
+    imprimir = imprimir.rstrip(', ')+'.'
+    return imprimir
+    
+def SelRaza():
+    '''Provee un selector de razas.'''
+    
+    raza = ''
+    razas = {'Humano':((0,0,0,0,0,0),'Mediano','humano',(),"30'"),
+             'Elfo':((0,2,-2,0,0,0),'Mediano','elfo',((3,2),(4,2),(14,2)),"30'"),
+             'Enano':((0,0,2,0,0,-2),'Mediano','enano',((1,2),(4,2),(39,2)),"20'"),
+             'Gnomo':((-2,0,2,0,0,0),'Pequeño','gnomo',((1,2),(14,2)),"20'"),
+             'Mediano':((-2,2,0,0,0,0),'Pequeño','mediano',((14,2),(21,2),(36,2),(41,2)),"20'"),
+             'Semielfo':((0,0,0,0,0,0),'Mediano','elfo',((3,1),(4,1),(8,2),(14,1),(25,2)),"30'"),
+             'Semiorco':((2,0,0,2,0,-2),'Mediano','orco',(),"30'")}
+    while raza not in razas:
+        raza = input('Raza: ').capitalize()
+        if raza not in razas:
+            print('Raza inválida o error ortográfico, intente nuevamente\n')
+    return razas[raza]
+
+def RepPunto(lista,Car):
+    '''Ordena la distribución de valores de característica.'''
+    
+    CarVal = 0
+    while CarVal == 0:
+        entrada = input(Car+': ')
+        if not entrada.isdigit():
+            print('Por favor ingrese sólo números\n')
+        elif int(entrada) not in lista:
+            print('No hay tiradas con ese valor\n')
+        else:
+            CarVal = int(entrada)
+            del lista[lista.index(int(entrada))]
+    return CarVal
+
+def CarMod(car):
+    '''Calcula el modificador de característica.'''
+    
+    if car % 2 == 0:
+        mod = (car-10)/2
+    else:
+        mod = (car-11)/2
+    return int(mod)
+
+def ProcApps (aptitudes):
+    '''Procesa las Aptitudes Especiales en formato CSV'''
+    
+    AE = {}
+    for A in aptitudes:
+        AE[A[0]]=A[1:]
+
+    for key in AE:
+        for i in range(len(AE[key])):
+            AE[key][i] = AE[key][i].split(',')
+
+    return AE
+
+def AppClas (APPdict,clase,nv_cls):
+    '''Devuelve las aptitudes de la clase al nivel dado.'''
+    
+    nv_cls -= 1
+    return APPdict[clase][nv_cls]
--- main.py
+++ main.py
@@ -1,75 +1,21 @@
-# coding=UTF-8
+﻿# coding=UTF-8
 
 from time import sleep
 import os
 import csv
-from funciones import *
+from func import *
 
 csv.register_dialect('myCSV',delimiter=';')
 
 # Set Up
-def leerCSV (archivo):
-    arch = csv.reader(open(archivo),dialect='myCSV')
-    temp = []
-    for t in arch:
-        temp.append(t)
-    if len(temp) == 1:
-        temp = temp[0]
-    return temp
-
-def claseas (clase):
-    hab_cls = {'Brb':(1,14,17,20,22,36,38,40,41),'Brd':(1,2,5,6,7,8,9,10,11,12,13,14,16,19,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,39,41),'Clr':(1,5,6,8,23,26,30,32,35,37),'Drd':(1,3,5,6,8,14,20,22,23,33,37,38,40,44),'Exp':(1,3,4,5,13,14,20,21,22,23,28,29,33,36,37,38,40,41,43,44),'Gue':(1,17,20,22,23,36,40,41),'Hcr':(1,5,6,10,23,26),'Mag':(1,5,6,7,23,26,27,28,29,30,31,32,33,34,35),'Mnj':(1,2,3,5,8,11,12,13,14,16,21,22,23,24,26,35,36,41),'Pld':(1,2,5,8,20,23,34,35,37,40),'Pcr':(0,1,4,7,8,9,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25,31,36,39,41,42,43)}
-    cls = []
-    for i in hab_cls[clase]:
-        cls.append(habs[i])
-    return cls
-
-def RepDot(lista_de_dotes,dtD):
-    lasdotes = []
-    nom = lista_de_dotes[0]
-    pre = lista_de_dotes[1]
-    des = lista_de_dotes[2]
-    ldt = []
-    while dtD > 0:
-        dt = input('Dote: ').rstrip(' ').capitalize()
-        while dt not in nom:
-            print ('Por favor, escribe la dote correctamente\n')
-            dt = input('Dote: ').rstrip(' ').capitalize()
-        if input('\n¿Desea conocer información sobre esta dote?\n').lower().startswith('s'):
-                print('\nPrerrequisitos: '+pre[nom.index(dt)]+'\n'+des[nom.index(dt)])
-        if input('\n¿Esta seguro?').lower().startswith('s'):
-            if nom.index(dt) in lasdotes:
-                if nom.index(dt) in dt_S:
-                    dtD -=1
-                    lasdotes.append(dotes.index(dt))
-                    ldt.append(dt)
-                elif nom.index(dt) in dt_ns:
-                    print ('Puedes elegir esta dote multiples veces, pero sus efectos no se apilan')
-                else:
-                    print ('No puedes elegir esa dotes dos veces, elige otra\n')
-            else:
-                dtD -=1
-                lasdotes.append(nom.index(dt))
-                ldt.append(dt)
-    return lasdotes,ldt
-
-def HabMod(mods,hab_num,FUE,DES,CON,INT,SAB,CAR):
-    mod = 0
-    temp = mods[hab_num]
-    if temp == 'FUE': mod = FUE
-    elif temp == 'DES': mod = DES
-    elif temp == 'CON': mod = CON
-    elif temp == 'INT': mod = INT
-    elif temp == 'SAB': mod = SAB
-    elif temp == 'CAR': mod = CAR
-    return rng[hab_num]+mod+rcl[hab_num]+sng[hab_num]+dts[hab_num]+obj[hab_num]
-
 Cars = ['Fuerza','Destreza','Constitución','Inteligencia','Sabiduría','Carisma']
 CARS = [0,0,0,0,0,0]
 
 DOTES = leerCSV('data/dotes.csv')
 habs = leerCSV('data/habs.csv')
 hab_mods = leerCSV('data/mods.csv')
+CLASES = leerCSV('data/clases.csv')
+APPS = ProcApps(leerCSV('data/apps.csv'))
 
 rng = [i*0 for i in range(len(habs))] # Rangos de habilidad
 dts = [i*0 for i in range(len(habs))] # Bonificadores por dotes
@@ -87,13 +33,11 @@ hab_n_E = [0,6,7,17,20,21,25,26,28,29,30,31,32,33,34,35,40,42,44]
 tamaño = {'Minúsculo':(+8,-16,+16),'Diminuto':(+4,-12,+12),'Menudo':(+2,-8,+8),'Pequeño':(+1,-4,+4),'Mediano':(+0,+0,+0),'Grande':(-1,+4,-4),'Enorme':(-2,+8,-8),'Gargantuesco':(-4,+12,-12),'Colosal':(-8,+16,-16)}
 
 #Inicio#
-tirs = GenTir()
-print('Sus tiradas son: '+PrepPrint(tirs))
+print('Sus tiradas son: '+GenTir())
 sleep (2)
 while input ('Desea tirar de nuevo?\n').lower().startswith('s'):
     os.system(['clear','cls'][os.name == 'nt'])
-    tirs = GenTir()
-    print('Sus nuevas tiradas son: '+PrepPrint(tirs))
+    print('Sus tiradas son: '+GenTir())
 
 print('\nSeleccione Raza (humano, enano, elfo, gnomo, mediano, semielfo o semiorco)\n')
 Raza = SelRaza()
@@ -104,256 +48,64 @@ tam_pres = tamaño[tam_nom][1]
 tam_esc = tamaño[tam_nom][2]
 raciales = Raza[3]
 
+print ('\nReparte tus puntuaciones de característica')
+for Car in Cars:
+    CARS[Cars.index(Car)]=RepPunto(tirs,Car)
+    
+for Car in Cars:
+    CARS[Cars.index(Car)]+Raza[0][Cars.index(Car)]
+
+FUE_mod = CarMod(CARS[0])
+DES_mod = CarMod(CARS[1])
+CON_mod = CarMod(CARS[2])
+INT_mod = CarMod(CARS[3])
+SAB_mod = CarMod(CARS[4])
+CAR_mod = CarMod(CARS[5])
+
+CARS_MODS = [FUE_mod,DES_mod,CON_mod,INT_mod,SAB_mod,CAR_mod]
+
 nv_pj = input('\nNivel del personaje?: ')
 while not nv_pj.isnumeric():
     print ('\nEl nivel debe ser un NUMERO entre 1 y 20')
     nv_pj = input('\nNivel del personaje?: ')
 nv_pj = int(nv_pj)
 
-print('\nNiveles de clase')
-clases = SelCla(nv_pj)
-proc = ProCla(clases)
-ATKbase = proc[0]
-Fortbase = proc[1]
-Refbase = proc[2]
-Volbase = proc[3]
+cla = [] ## ['Gue', '', 'Mag']
+lasclases = [] ## ['Guerrero', 'Guerrero', 'Mago']
+dotes = []
+stats = [0,0,0,0]
 
-print ('\nReparte tus puntuaciones de característica')
-for Car in Cars:
-    CARS[Cars.index(Car)]=RepPunto(tirs,Car)
-
-lasclases = []
-temp = []
-for cla in clases:
-        if cla not in lasclases:
-            if cla == 'Brb':clase = 'Bárbaro'
-            elif cla == 'Brd':clase = 'Bardo'
-            elif cla == 'Clr':clase = 'Clérigo'
-            elif cla == 'Drd':clase = 'Druida'
-            elif cla == 'Exp':clase = 'Explorador'
-            elif cla == 'Gue':clase = 'Guerrero'
-            elif cla == 'Hcr':clase = 'Hechicero'
-            elif cla == 'Mag':clase = 'Mago'
-            elif cla == 'Mnj':clase = 'Monje'
-            elif cla == 'Pld':clase = 'Paladín'
-            elif cla == 'Pcr':clase = 'Pícaro'
-            if clase not in temp:
-                lasclases.append(clase)
-
-## A partir de este punto, clases contiene la lista corta 'gue','gue','mag'
-## mientras que lasclases contiene la lista con el nombre completo: 'Guerrero','Guerrero','Mago'
-
-## Aumento de Características ##
-os.system(['clear','cls'][os.name == 'nt'])
-if nv_pj >= 4:
-    print ('Tu personaje es de nivel superior a 4. Tienes'+round(nv_pj/4)+' aumentos de características')
-    for i in range(1, nv_pj+1):
-        if i%4==0:
-            punt = 1
-            for Car in Cars:
-                inpunt = input (Car+' '+str(CARS[Cars.index(Car)])+'+')
-                while int(inpunt) > punt:
-                    print ('No dispones de esos puntos')
-                    inpunt = input (Car+' '+str(CARS[Cars.index(Car)])+'+')
-                while int(inpunt) < 0:
-                    print ('No puedes restar puntos')
-                    inpunt = input (Car+' '+str(CARS[Cars.index(Car)])+'+')
-                if int(inpunt) > 0:
-                    CARS[Cars.index(Car)] += punt
-                    punt -= 1
-
-
-fFUE = CARS[0]+Raza[0][0]
-fDES = CARS[1]+Raza[0][1]
-fCON = CARS[2]+Raza[0][2]
-fINT = CARS[3]+Raza[0][3]
-fSAB = CARS[4]+Raza[0][4]
-fCAR = CARS[5]+Raza[0][5]
-
-os.system(['clear','cls'][os.name == 'nt'])
-if nv_pj > 4:
-    print('Tus puntuaciones de característica finales son:')
-    for Car in Cars:
-        print (Car+' '+str(CARS[Cars.index(Car)]+Raza[0][Cars.index(Car)]))
-
-FUE_mod = CarMod(fFUE)
-DES_mod = CarMod(fDES)
-CON_mod = CarMod(fCON)
-INT_mod = CarMod(fINT)
-SAB_mod = CarMod(fSAB)
-CAR_mod = CarMod(fCAR)
-                
-PHs = PuntosHab(INT_mod,clases)
-DotesNivel = DtsNivel(clases)
-velocidad = Raza[4]
-
-Iniciativa = DES_mod
-ATKFinalCC = ATKbase + FUE_mod
-ATKFinalAD = ATKbase + DES_mod
-Presa = ATKbase + FUE_mod + tam_pres
-Fort = Fortbase + CON_mod
-Ref = Refbase + DES_mod
-Vol = Volbase + SAB_mod
-PG = PG(CON_mod,clases)
-
-if Raza[2] == 'humano':
-    for i in PHs:
-        if PHs.index(i) == 0:
-            PHs[PHs.index(i)]+=4
-        else:
-            PHs[PHs.index(i)]+=1
-elif Raza[2] == 'mediano':
-    Fort += 1
-    Ref += 1
-    Vol += 1
-#########################################3
-        
-temp = list(range(len(PHs)))
-for i in temp:
-    PH = PHs[i]
+for nivel in range(1,nv_pj+1):
     os.system(['clear','cls'][os.name == 'nt'])
-    print('\nTienes '+str(PH)+' puntos de habilidad para distribuir en este nivel.\n')
-    b = ''
-    if input('Deseas conocer tus habilidades de clase? ('+lasclases[temp.index(i)]+')\n').lower().startswith('s'):
-        for hab in claseas(clases[temp.index(i)]):
-            b = b+hab+', '
-        print(b.rstrip(', ')+'.\n')
-
-    print ('Recuerda que cualquier habildiad transclásea cuesta dos puntos en lugar de uno.\nEscribe una habilidad y los puntos que desees invertir en ella.\n')
-
-    rng_max = nv_pj+3
-    rng_max_tc = round(rng_max/2)
-    while PH > 0:
-        hab = input('\nHabilidad: ').rstrip(' ').capitalize()
-        while hab not in habs:
-            print('Por favor, escribe la habilidad correctamente')
-            hab = input('\nHabilidad: ').rstrip(' ').capitalize()
-        if rng[habs.index(hab)] > 0:
-            print (hab+' ya posee '+str(rng[habs.index(hab)])+' rangos.')
-        puntos = input('Puntos: ')
-        while not puntos.isnumeric():
-            print ('Los rangos deben ser numéricos')
-            puntos = input('Puntos: ')
-        while int(puntos) > PH:
-            print('No posees tantos puntos de habilidad')
-            puntos = input('Puntos: ')
-        puntos = int(puntos)
-        
-        clase = clases[i]
-        if hab not in claseas(clase):
-            PH -= puntos
-            rng[habs.index(hab)] += puntos/2
-            print('\nPuntos restantes: '+str(PH))
-            if PH < 0:
-                print('no alcanzan los puntos')
-                PH += puntos*2
-                rng[habs.index(hab)] -= puntos/2
-        else:
-            if rng[habs.index(hab)] + puntos > rng_max:
-                print(hab+' ha alcanzado el rango máximo ('+str(rng_max)+').')
-                PH -= rng_max
-                rng[habs.index(hab)] += rng_max
-                print('\nPuntos restantes: '+str(PH))
-                if PH < 0:
-                    print('no alcanzan los puntos')
-                    PH += rng_max
-                    rng[habs.index(hab)] -= rng_max
-            else:
-                PH -= puntos
-                rng[habs.index(hab)] += puntos
-                print('\nPuntos restantes: '+str(PH))
-                if PH < 0:
-                    print('no alcanzan los puntos')
-                    PH += puntos
-                    rng[habs.index(hab)] -= puntos
-
-print('\nElige dotes')
-if Raza[2] == 'humano':
-    dotes = RepDot (DOTES,DotesNivel+1)
-else:
-    dotes = RepDot (DOTES,DotesNivel)
-
-# Aplicar modificadores
-# Raciales
-for r in raciales:
-    rcl[r[0]]+=r[1]
-
-# Por Dotes
-for d in dotes[0]:
-    if d in dt_hab:
-        for i in dt_hab[d]:
-            dts[i]+=2
-
-# Por Objetos
-## aún no implementado
-
-# Por sinergías
-for r in rng:
-    if rng.index(r) in hab_sin:
-        for i in hab_sin[rng.index(r)]:
-            sng[i]+=2
-
-# Ordenar las habilidades que se van a imprimir
-temp = []
-t = -1
-imprimir = ''
-
-# Rangos de habilidad
-for r in rng:
-    t+=1
-    if rng[t]>0:
-        if habs[t] not in temp:
-            temp.append(habs[t])
-
-# Bonificadores raciales
-t = -1
-for r in rcl:
-    t+=1
-    if rcl[t]>0:
-        if habs[t] not in temp:
-            temp.append(habs[t])
-
-# Bonificadores de sinergia
-t = -1
-for r in sng:
-    t+=1
-    if sng[t]>0:
-        if habs[t] not in temp:
-            temp.append(habs[t])
-
-# Bonificadores por dotes
-t = -1
-for r in dts:
-    t+=1
-    if dts[t]>0:
-        if habs[t] not in temp:
-            temp.append(habs[t])
-
-temp.sort()
-
-# Eliminar las habilidades 'solo entrenadas' sin rangos
-for h in temp:
-    if rng[habs.index(h)] == 0:
-        if habs.index(h) in hab_n_E:
-            del temp[temp.index(h)]
-
-for h in temp:
-    imprimir = imprimir+h+' +'+str(HabMod(hab_mods,habs.index(h),FUE_mod,DES_mod,CON_mod,INT_mod,SAB_mod,CAR_mod))+', '
-imprimir = imprimir.rstrip(', ')+'.'
-
-#Output
-if input('Deseas Guardar este personaje?\n').lower().startswith('s'):
-    nombre = input('\nNombre: ').capitalize()
-    Pj = open('Personajes/'+nombre+'.txt','w')
-    Pj.write('Nombre: '+nombre+'\n')
-    Pj.write('Tipo y Tamaño: Humanoide '+tam_nom+' ('+subtipo+')\n')
-    Pj.write('DG: '+PG[1]+' ('+str(PG[0])+' pg)\n')
-    Pj.write('Iniciativa: +'+str(Iniciativa)+'\nVelocidad: '+velocidad+'\n\n')
-    Pj.write("Ataque base: +"+str(ATKbase)+"\nPresa: +"+str(Presa)+"\n\nE/A: 5'/5'.\n\n\n")
-    Pj.write('TS: Fortaleza +'+str(Fort)+', Reflejos +'+str(Ref)+', Voluntad +'+str(Vol)+'\n')
-    Pj.write('Características: Fuerza '+str(fFUE)+', Destreza '+str(fDES)+', Constitución '+str(fCON)+', Inteligencia '+str(fINT)+', Sabuduría ' + str(fSAB)+', Carisma '+str(fCAR)+'.\nHabilidades y Dotes: '+imprimir+' '+PrepPrint(dotes[1]))
-    sleep(3)
-    print('\nPersonaje Guardado')
-    Pj.close()
-print ('Gracias')
-sleep (2)
+    print ('~~ '+str(nivel)+'º NIVEL ~~\n')
+    ## Elegir Clase ##
+    if nivel == 1:
+        clase = SelCla('')
+    else:
+        clase = SelCla(lasclases[nivel-2])
+    cla.append(clase)
+    lasclases.append(CLASES[5][CLASES[0].index(clase)])
+    for i in range(len(cla)):
+        if cla[i] == '':
+            cla[i] = cla[i-1]
+    for i in range(4):
+        stats[i] += ProCla(CLASES,clase,cla.count(clase))[i]
+    
+    ## Aumento de Características en niveles multiplos de 4 ##
+    if nivel %4==0: 
+        print ('\nEn el '+str(nivel)+'º nivel, tienes un aumento de características')
+        print ('Selecciona la característica que quieres aumentar')
+        Car = input('Característica: ').capitalize()
+        while Car.capitalize() not in Cars:
+            print ('La característica es inválida o inexistente')
+            Car = input('Característica: ').capitalize()
+        CARS[Cars.index(Car)]+=1
+        print ('El personaje tiene ahora '+Car+' '+str(CARS[Cars.index(Car)])+' (+'+str(CarMod(CARS[Cars.index(Car)]))+')')
+    
+    ## Cáculo de puntos y Asignación de Rangos de habilidad ##
+    hab_rng = RepRNG (PuntHab (CLASES,clase,nivel,INT_mod,subtipo),cla.count(clase),Claseas(clase),habs)
+    for i in hab_rng:
+        rng[habs.index(i)] += hab_rng[i]
+    
+    ## Elección de Dotes ##
+    dotes.append(SelDot(DOTES,nivel))
